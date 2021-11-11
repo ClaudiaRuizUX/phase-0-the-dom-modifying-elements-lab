9f6e101f75acf48d1848c20c292dc86f01b77e0a
Claudia <claudiaruizux@gmail.com>
2021-11-11 20:32:36 +1100

    Completed Modifying Elements

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+
+
+document.querySelector('main#main').remove();
+
+
+
+
+
+
+// index.html
+// after index.js is processed
+//   1) no longer has DOM node 'main#main'
+//   2) has a 'newHeader' variable that points to node 'h1#victory'
+//   3) has a 'newHeader' variable that points to node 'h1#victory'
+//   4) has a 'newHeader' variable that points to node 'h1#victory' with "YOUR-NAME is the champion" inside
+
+
+
+// describe("after index.js is processed", () => {
+//     it("no longer has DOM node 'main#main'", () => {
+//       expect(document.querySelector('main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+//     });
